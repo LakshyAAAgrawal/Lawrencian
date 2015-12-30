2536cd7c336aeb9d9987e04e23ca77103cdd9b9e
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2015-12-30 22:39:26 +0530

    working xml parse

---
+++ src/com/lakagr/oss/Lawrence/EventItem.java
@@ -0,0 +1,10 @@
+package com.lakagr.oss.Lawrence;
+import java.lang.String;
+public class EventItem{
+	boolean isText;
+	String val;
+	public EventItem(boolean isText,String val){
+		this.isText=isText;
+		this.val=val;
+	}
+}
--- src/com/lakagr/oss/Lawrence/Lawrence.java
+++ src/com/lakagr/oss/Lawrence/Lawrence.java
@@ -1,7 +1,18 @@
 package com.lakagr.oss.Lawrence;
-
+import com.lakagr.oss.Lawrence.*;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.File;
+import android.os.Environment;
+import android.widget.Toast;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.IOException;
+import android.util.Log;
 
 public class Lawrence extends Activity
 {
@@ -14,13 +25,59 @@ public class Lawrence extends Activity
         final String[] values=new String[]{	"Welcome to The Lawrence School, Lovedale",
         					"A computer quiz was organised for the Senior School", 
         					"wehfhefhwehfuwhefowhefohewfh", 
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
+        					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf",
         					"uihfuiowhef89wyefefyhfyhweyhfhweuifhiuf"
         				  };
-        final ArrayList<String> list = new ArrayList<String>();
-        for(int i=0;i<values.length;i++){
-        	list.add(values[i]);
+        				  Log.d("Lawrence Activity","initialising araylist inputsream");
+	ArrayList<EventItem> a=null;
+	InputStream stream=null;
+       	try{
+       		Log.d("Lawrence","before donwload");
+       		 stream=download("http://www.randomsongs.hostei.com/test.xml");
+       		 Log.d("Lawrence","after download");
+        	 a=LawrenceXmlParser.parse(stream);
+        	 stream.close();
+        }catch(Exception e){
+        	Toast.makeText(getApplicationContext(), "Internet Error",Toast.LENGTH_LONG).show();
+        	Log.d("Lawrence","internet");
+        	e.printStackTrace();
         }
-        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,list);
-        listview.setAdapter(adapter);
+        Log.d("Lawrence Activity","initialised araylist inputsream");
+        final ArrayList<String> list=new ArrayList<String>();
+        Log.d("Lawrence Activity","initialised list");
+        for(int i=0;i<a.size();i++){
+        	Log.d("Lawrence Activity"," "+i);
+        	list.add(a.get(i).val);
+        }
+        Log.d("Lawrence Activity","added values");
+        final ArrayAdapter adapter = new ArrayAdapter(this, R.layout.simple_list_item_1, R.id.textview,list);
+        ((ListView)findViewById(R.id.listview)).setAdapter(adapter);
+        Log.d("Lawrence Activity","set adapter");
+    }
+    private InputStream download(String urlString) throws IOException{
+    	Log.d("Lawrence","before url");
+    	URL url=new URL(urlString);
+    	Log.d("Lawrence","http initialising");
+    	HttpURLConnection conn=(HttpURLConnection)url.openConnection();
+    	/*
+    	Log.d("Lawrence","http initialised");
+    	conn.setReadTimeout(10000);
+    	conn.setConnectTimeout(15000);
+    	conn.setRequestMethod("GET");
+    	
+    	Log.d("Lawrence","connecting");
+    	conn.connect();
+    	Log.d("Lawrence","connected");
+    	*/
+    	return conn.getInputStream();
     }
 }
